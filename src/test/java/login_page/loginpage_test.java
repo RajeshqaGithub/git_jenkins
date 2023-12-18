aa84211dcff94c5db2eaf0a0b698d82250fafbfa
rajeshqagit <rajesh.samalqa@gmail.com>
2023-12-19 00:21:12 +0530

    Message

---
+++ src/test/java/login_page/loginpage_test.java
@@ -0,0 +1,37 @@
+package login_page;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class loginpage_test {
+	WebDriver driver;
+	
+	@BeforeMethod
+	public void setup() throws Throwable
+	{
+		driver= new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		Thread.sleep(3000);
+	}
+	@Test
+	public void verifyHomePageTitleTest() throws Throwable
+	{
+		driver.findElement(By.xpath("//input[@placeholder='Username']")).sendKeys("Admin");
+		driver.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys("admin123");
+		driver.findElement(By.xpath("//button[normalize-space()='Login']")).click();
+		Thread.sleep(3000);
+	}
+	@AfterMethod
+	public void tearDown()
+	{
+		driver.quit();
+	}
+	
+
+}
